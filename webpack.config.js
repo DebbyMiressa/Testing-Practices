2f0d4264fc154cd5208dcf48876f4b5aea82b43b
Debby Miressa <101793279+DebbyMiressa@users.noreply.github.com>
2022-08-20 20:00:19 +0300

    Setup Repository
    - Install Web-Pack and HTML-plugin
    - Update README.md

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
